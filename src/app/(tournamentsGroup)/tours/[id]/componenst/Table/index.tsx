3e9c25a69c14aa81b937549e3e33445977186087
Тимерьян Насибуллин <-github-email@example.com>
2024-03-29 21:42:58 +0500

    feat: add layout for table

--- src/app/(tournamentsGroup)/tours/[id]/componenst/Table/index.tsx
+++ src/app/(tournamentsGroup)/tours/[id]/componenst/Table/index.tsx
@@ -1,10 +1,45 @@
+"use client";
+
+import { Bracket, IRoundProps } from "react-brackets";
 import S from "./Table.module.scss";
+
+const rounds: IRoundProps[] = [
+  {
+    title: "Round one",
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: "Team A" }, { name: "Team B" }],
+      },
+      {
+        id: 2,
+        date: new Date().toDateString(),
+        teams: [{ name: "Team C" }, { name: "Team D" }],
+      },
+    ],
+  },
+  {
+    title: "Round one",
+    seeds: [
+      {
+        id: 3,
+        date: new Date().toDateString(),
+        teams: [{ name: "Team A" }, { name: "Team C" }],
+      },
+    ],
+  },
+];
 
 const Table = () => {
   return (
     <div className={S.wrap}>
       <div>
         <span className={S.title}>Турнирная сетка</span>
+      </div>
+
+      <div>
+        <Bracket rounds={rounds} />
       </div>
     </div>
   );
